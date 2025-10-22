6d524aab6e5a674f051e366b1c759b4a355855ad
danielgarciajulio2023-arch <danielgarciajulio2023@gmail.com>
2025-10-22 20:54:04 +0000

    trabajos de hoy clases

--- Mascota.java
+++ Mascota.java
@@ -1,23 +1,28 @@
+//la clase lleva un nombre publico al nombre del archivo en igual al nombre de la clase
 public class Mascota{
-//Declaro los atributos de la clase u objeto
+//1.Atributos cuantos?...o hay limite los que necesite mi proyecto en este case 3
+
 private String nombre;
 private String tipo;
 private String color;
 
+//constructor, la maquina
 public Mascota(String nombredemimascota,String tipomascota,String colormascota){
  this.nombre=nombredemimascota;
  this.tipo=tipomascota;
  this.color=colormascota;
 }
-
+//3.funciones de trabajo del objeto
 public void Saludar(){
     System.out.println("Hola, soy "+nombre+"! y soy de tipo: "+tipo+"! y soy de color "+color+"!");
 }
 
 //Creando herencia de la clase Mascota con extends
+//clase hija interna (clase para crear otra y heredar atributos,superclase)
 public class tipoanimalmascota extends Mascota {
     private String animal;
 
+    //constructor de superclase
   public tipoanimalmascota ( String nombre,String tipo, String color,String raza){
     super(nombre,tipo,color);
     this.animal = animal;
@@ -25,12 +30,13 @@ public class tipoanimalmascota extends Mascota {
     
   }
 
+   //Metodo de superclase
   public void  definirtipoanimal(){
     System.out.println("Soy un " + animal);  
   }  
 } 
 
-
+//
 public static void main(String[] args){
     Mascota MascotaHija=new Mascota("Nala","bichon martes","blanca");//llamo al constructor
     MascotaHija.Saludar();
